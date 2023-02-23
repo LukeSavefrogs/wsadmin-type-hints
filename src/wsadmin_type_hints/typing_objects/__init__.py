e6bb0a35be00525d16ce50cec238213d6df073f7
Luca Salvarani <lucasalvarani99@gmail.com>
2023-02-23 06:04:48 +0100

    docs: Change admonition title

--- src/wsadmin_type_hints/typing_objects/__init__.py
+++ src/wsadmin_type_hints/typing_objects/__init__.py
@@ -1,7 +1,7 @@
 """
 This module is used internally by `wsadmin-type-hints` to define appropriate return types for some `wsadmin` methods.
 
-Warning:
+Warning: READ ME
     These are NOT real classes, so they can't be used as-is.
 
     The **actual return type** is the one they inherit from (_for example `RunningObjectName` inherits from `str`, 
